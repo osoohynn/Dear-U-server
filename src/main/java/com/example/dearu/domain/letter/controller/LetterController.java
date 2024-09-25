31207d3e29abdbe678cabb7d5f781e9d107ff821
su080915 <tbr335@naver.com>
2024-09-25 20:51:30 +0900

    feat: change index message

--- src/main/java/com/example/dearu/domain/letter/controller/LetterController.java
+++ src/main/java/com/example/dearu/domain/letter/controller/LetterController.java
@@ -1,10 +1,13 @@
 package com.example.dearu.domain.letter.controller;
 
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LetterController {
+
+    @GetMapping
     public String index() {
         return "성공";
     }
