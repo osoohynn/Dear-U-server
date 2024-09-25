8a572f819334e5aab89770551fed38314873f7e3
su080915 <tbr335@naver.com>
2024-09-25 19:45:29 +0900

    chore: ad deploy yml

---
+++ src/main/java/com/example/dearu/domain/letter/controller/LetterController.java
@@ -0,0 +1,11 @@
+package com.example.dearu.domain.letter.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LetterController {
+    public String index() {
+        return "성공";
+    }
+}
