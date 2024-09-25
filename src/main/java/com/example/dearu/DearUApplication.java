a441e62a27fa454e93512f81403ffe8c162df364
su080915 <tbr335@naver.com>
2024-09-25 16:31:00 +0900

    initial commit

---
+++ src/main/java/com/example/dearu/DearUApplication.java
@@ -0,0 +1,13 @@
+package com.example.dearu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearUApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DearUApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dearu/DearUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dearu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearUApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
